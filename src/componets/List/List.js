ee31485cac05f079a635a61f43eff54c663b4328
robynroby <robinsonmwaura254@gmail.com>
2023-05-06 18:42:30 +0300

    :modified list.js

--- src/componets/List/List.js
+++ src/componets/List/List.js
@@ -1,71 +1,70 @@
-import React from 'react'
-import './List.css'
+import React, { useState } from 'react';
+import './List.css';
 import houseImage from '../../images/Bitmap.png';
 
-import { Link } from 'react-router-dom';
-
 function List() {
-  return (<div className='cardholder'>
-    
-      <div className="card">
+  const [selectedCard, setSelectedCard] = useState(null);
+  const handleCardClick = (index) => {
+    setSelectedCard(index);
+  }
+
+  const cardList = [
+    {title: 'Beautiful House', address: '123 Main St, Anytown USA', price: 'Ksh 30m'},
+    {title: 'Nice House', address: '456 Main St, Anytown USA', price: 'Ksh 40m'},
+    {title: 'Big House', address: '789 Main St, Anytown USA', price: 'Ksh 50m'},
+    {title: 'Small House', address: '101 Main St, Anytown USA', price: 'Ksh 20m'},
+    {title: 'Cozy House', address: '202 Main St, Anytown USA', price: 'Ksh 25m'},
+    {title: 'Cozy House', address: '202 Main St, Anytown USA', price: 'Ksh 25m'},
+    {title: 'Cozy House', address: '202 Main St, Anytown USA', price: 'Ksh 25m'},
+    {title: 'Cozy House', address: '202 Main St, Anytown USA', price: 'Ksh 25m'},
+  ];
+
+  const renderCard = (card, index) => {
+    return (
+      <div key={index} className="card" onClick={() => handleCardClick(index)}>
         <div className="house-image" style={{backgroundImage: `url(${houseImage})`}}></div>
         <div className="house-details">
-          <h2>Beautiful House</h2>
-          <p>123 Main St, Anytown USA</p>
+          <h2>{card.title}</h2>
+          <p>{card.address}</p>
         </div>
         <div className="house-price">
-          <h3>Ksh 30m</h3>
+          <h3>{card.price}</h3>
         </div>
       </div>
+    );
+  }
 
-      <div className="card">
-        <div className="house-image" style={{backgroundImage: `url(${houseImage})`}}></div>
-        <div className="house-details">
-          <h2>Beautiful House</h2>
-          <p>123 Main St, Anytown USA</p>
-        </div>
-        <div className="house-price">
-          <h3>Ksh 30m</h3>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="house-image" style={{backgroundImage: `url(${houseImage})`}}></div>
-        <div className="house-details">
-          <h2>Beautiful House</h2>
-          <p>123 Main St, Anytown USA</p>
-        </div>
-        <div className="house-price">
-          <h3>Ksh 30m</h3>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="house-image" style={{backgroundImage: `url(${houseImage})`}}></div>
-        <div className="house-details">
-          <h2>Beautiful House</h2>
-          <p>123 Main St, Anytown USA</p>
-        </div>
-        <div className="house-price">
-          <h3>Ksh 30m</h3>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="house-image" style={{backgroundImage: `url(${houseImage})`}}></div>
-        <div className="house-details">
-          <h2>Beautiful House</h2>
-          <p>123 Main St, Anytown USA</p>
-        </div>
-        <div className="house-price">
-          <h3>Ksh 30m</h3>
-        </div>
-      </div>
-    
-
+  return (
+    <div className='cardholder'>
+      {cardList.map((card, index) => {
+        return index === selectedCard ? (
+          <div key={index}>
+            {renderCard(card, index)}
+            <div className='form-container'>
+              <form>
+                <h2>Fill in your details:</h2>
+                <label>
+                  Name:
+                  <input type="text" name="name" />
+                </label>
+                <label>
+                  Email:
+                  <input type="email" name="email" />
+                </label>
+                <label>
+                  Phone:
+                  <input type="tel" name="phone" />
+                </label>
+                <button type="submit">Submit</button>
+              </form>
+            </div>
+          </div>
+        ) : (
+          renderCard(card, index)
+        );
+      })}
     </div>
-    
-  )
+  );
 }
 
-export default List
+export default List;
